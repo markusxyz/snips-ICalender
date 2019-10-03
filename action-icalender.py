c08ee7b41302bd8671a6e6fdc328c36219d596cb
markusxyz <markus_schorsch@hotmail.com>
2019-10-03 17:18:10 +0100

    Change to Python 3

--- action-icalender.py
+++ action-icalender.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 from snipsTools import SnipsConfigParser
 from hermes_python.hermes import Hermes
 from hermes_python.ontology import *
 import io
-from urllib import urlopen
+from urllib.request import urlopen
 import datetime
 
 CONFIG_INI = "config.ini"
@@ -24,12 +24,12 @@ class ICalender(object):
     """
 
     def __init__(self):
-        # get the configuration if needed
+                # get the configuration if needed
         try:
-            self.config = SnipsConfigParser.read_configuration_file(CONFIG_INI)
-	    self.url =  self.config.get("secret").get("url")
+                self.config = SnipsConfigParser.read_configuration_file(CONFIG_INI)
+                self.url =  self.config.get("secret").get("url")
         except :
-            self.config = None
+                self.config = None
 
         # start listening to MQTT
         self.start_blocking()
@@ -40,44 +40,44 @@ class ICalender(object):
         hermes.publish_end_session(intent_message.session_id, "")
         
         # action code goes here...
-        print '[Received] intent: {}'.format(intent_message.intent.intent_name)
-	c = urlopen(self.url).read()
-	print (self.url)
-	startstop = False
-	tree = []
+        print('[Received] intent: {}'.format(intent_message.intent.intent_name))
+        c = urlopen(self.url).read().decode()
+        print (self.url)
+        startstop = False
+        tree = []
 
-	today = datetime.date.today()
-	tomorrow = today + datetime.timedelta(1)
-	yesterday = today + datetime.timedelta(-1)
+        today = datetime.date.today()
+        tomorrow = today + datetime.timedelta(1)
+        yesterday = today + datetime.timedelta(-1)
 
-	datum = today.strftime("%Y%m%d")
+        datum = today.strftime("%Y%m%d")
 
-	say = ''
+        say = ''
 
-	for line in c.splitlines():
+        for line in c.splitlines():
     
-    		liste = line.split(':')
-    
-    		if ( liste[0] == 'BEGIN' and liste[1] == 'VEVENT'):
-        		startstop = True
-        		tupel = []
+                liste = line.split(':')
+
+                if ( liste[0] == 'BEGIN' and liste[1] == 'VEVENT'):
+                        startstop = True
+                        tupel = []
         
-    		if ( liste[0] == 'END' and liste[1] == 'VEVENT'):
-        		startstop = False
-        		tree.append(tupel)
+                if ( liste[0] == 'END' and liste[1] == 'VEVENT'):
+                        startstop = False
+                        tree.append(tupel)
         
-    		if ( startstop ):
-        		if ( len(liste) > 1 ):
-            			tupel.append(liste[1])
+                if ( startstop ):
+                        if ( len(liste) > 1 ):
+                                tupel.append(liste[1])
+        for each in tree:
 
-	for each in tree:
-    		if ( each[4].startswith(datum)):
+                if ( each[4].startswith(datum)):
+                        
+                        zeit = each[4].split('T')
+                        zeit = str(zeit[1])
 
-        		zeit = each[4].split('T')
-        		zeit = str(zeit[1])
-
-        		say = say + ' ' + each[7] + ' ' + zeit[0:2] + ":" + zeit[3:5] + " Uhr"
-        		print (say)
+                        say = say + ' ' + each[7] + ' ' + zeit[0:2] + ":" + zeit[3:5] + " Uhr"
+                        print (say)
 
         # if need to speak the execution result by tts
         hermes.publish_start_session_notification(intent_message.site_id, say, "")
